612af44922d5c352cd3553184083b6088fc65621
Wilton <wiltoncamilol@gmail.com>
2022-11-04 18:00:56 -0400

    finished results

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {boolean} Returns `true` if all values in the array are numbers. Otherwise, return `false`.
  */
-function isValid(rolls) {}
+function isValid(rolls) {
+  if(rolls.length === 0){
+    return true
+  } else {
+    for (let i = 0; i < rolls.length; i++) {
+       if(Number.isInteger(rolls[i])){
+        return true
+       } else {
+        return false
+       }
+      
+    }
+  }
+}
+
 
 /**
  * Finds a value in an array. If that value is in the array, returns it. Otherwise, returns `null`.
@@ -11,22 +25,48 @@ function isValid(rolls) {}
  * @param {number} value - A specific value to find.
  * @returns {*} - The found value or `null`.
  */
-function findValue(rolls, value) {}
-
+function findValue(rolls, value) {
+  if(rolls.length === 0){
+    return null
+  } else {
+  for (let i = 0; i < rolls.length; i++) {
+    if(rolls.includes(value)){
+      return value
+    } else {
+      return null
+    }
+    }
+  }
+}
 /**
  * Returns a new array from the `rolls` array with only values equal to or greater than the `lowest` value.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @param {number} lowest - A number that represents the lowest allowed value in the new array.
  * @returns {number[]} An array of all numbers that are equal to or higher than the `lowest` value.
  */
-function filterOutLowValues(rolls, lowest) {}
+function filterOutLowValues(rolls, lowest) {
+  newArray = []
+
+  for (let i = 0; i < rolls.length; i++) {
+    if(rolls[i] >= lowest){
+      newArray.push(rolls[i])
+    }
+  }
+return newArray
+}
 
 /**
  * Returns an object which has rolls as keys and counts as values.
  * @param {number[]} rolls - An array of numbers representing rolls on a die.
  * @returns {object} An object where the keys are numbers rolled and the values are the number of times that roll appears in the `rolls` array.
  */
-function getRollCounts(rolls) {}
+function getRollCounts(rolls) {
+  newObj = {}
+
+  for (let i = 0; i < rolls.length; i++) {
+    
+  }
+}
 
 // Do not change the code below here.
 module.exports = {
